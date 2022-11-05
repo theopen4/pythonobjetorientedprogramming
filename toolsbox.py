0a32459a63f2a8ce1ea2d39893a7dbe5996c267b
theopen4 <kielze86@gmail.com>
2022-11-05 09:51:42 +0100

    first commit

---
+++ toolsbox.py
@@ -0,0 +1,50 @@
+class Toolsbox:
+    """difine one toolsbox"""
+    def __init__(self):
+        #init tools
+        self.tools = []
+
+        #add tool
+    def add_tool(self,tool) :
+        pass  
+
+    def remove_tool(self,tool):
+        #remove tools 
+        pass
+
+
+class Hammer:
+    """define hammer"""
+    def __init__(self,color="red") :
+        #init colors
+        self.color = color
+
+    def paint(self,color):
+        """paint hammer"""
+        self.color = color
+        pass
+
+    def hammer_in(self,nail):
+        """drive the nail"""
+        pass
+
+    def remove_nail(self,nail):
+        """remove nail"""
+        pass
+
+        class screwdriver:
+            """define screwdriver"""
+            def __init__(self, size):
+                # init size
+                self.size = size
+
+            def tighten_screw(self,screw):
+                """tighten screw"""    
+                pass
+            def loosen_screw(self,screw):
+                """loosen screw"""
+                pass
+        
+
+
+        
